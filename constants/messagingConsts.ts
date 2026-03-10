b2ddadf129192a722e1ea284e896886fce6a1d90
udi <udilub@gmail.com>
2026-03-10 12:39:05 +0200

    add new pom elements and functions

---
+++ constants/messagingConsts.ts
@@ -0,0 +1,4 @@
+export const messagingConsts = {
+    MESSAGE_INPUT_PLACEHOLDER: 'Type your message...',
+    MESSAGE_COUNT_INITIAL: 0,
+}
--- pages/chatPage.ts
+++ pages/chatPage.ts
@@ -1,13 +1,47 @@
-
-import { Page } from '@playwright/test';
+import { Page, Locator } from '@playwright/test';
 import path from 'path';
 
 export class ChatPage {
-  constructor(private page: Page) {}
+  constructor(private page: Page) {
+    this.page = page;
+  }
+
+  // Element locators
+  get messageInput(): Locator {
+    return this.page.locator('#messageInput');
+  }
+
+  async getMessageInputPlaceholder(): Promise<string | null> {
+    return this.messageInput.getAttribute('placeholder');
+  }
+
+  get sendButton(): Locator {
+    return this.page.locator('#sendBtn');
+  }
+
+  get messageList(): Locator {
+    return this.page.locator('#messageList');
+  }
+
+  get messages(): Locator {
+    return this.page.locator('#messageList li');
+  }
 
   async navigate() {
     const filePath = `file://${path.resolve(__dirname, '../app/dummy-messaging-app.html')}`;
     await this.page.goto(filePath);
   }
-  
+
+  async typeMessage(text: string) {
+    await this.messageInput.fill(text);
+    console.log('Message input filled with text: ', text);
+  }
+
+  async clickSend() {
+    await this.sendButton.click();
+  }
+
+  async getMessages() {
+    return this.messages.allTextContents();
+  }
 }
