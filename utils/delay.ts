c707946b6f467fdcebfac7d31ef8a23a9a0d11e6
udi <udilub@gmail.com>
2026-03-11 17:17:45 +0200

    update constants

--- utils/delay.ts
+++ utils/delay.ts
@@ -1,5 +1,5 @@
 import { messagingConsts } from '@constants/messagingConsts';
 
-export async function delay(ms: number = messagingConsts.SHORT_DELAY_MS): Promise<void> {
+export async function delay(ms: number = messagingConsts.timing.SHORT_DELAY_MS): Promise<void> {
     await new Promise(resolve => setTimeout(resolve, ms));
 }
